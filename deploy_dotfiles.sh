91a94ed1ea7a5e3b7cd56a50001bb88ee8e67a09
your name <you@example.com>
2019-08-03 22:12:22 +0900

    Modify dotfile-deploy script and add a naive ubuntu setup script

--- deploy_dotfiles.sh
+++ deploy_dotfiles.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-CWD=`pwd`
 echo "Deploying dotfiles ..."
 
+CWD=$(pwd)
 for f in * .*; do
   [ "$f" = "." ] && continue
   [ "$f" = ".." ] && continue
   [ "$f" = ".git" ] && continue
   [ "$f" = ".gitignore" ] && continue
+  [ "$f" = ".config" ] && continue
   [[ $f = *.md ]] && continue
   [[ $f = *.sh ]] && continue
+  [[ $f = *.txt ]] && continue
 
   if [ -e $HOME/$f ]; then
     echo "Cannot deploy $f: $HOME/$f already exists"
@@ -17,5 +19,18 @@ for f in * .*; do
     ln -s $CWD/$f $HOME/$f
   fi
 done
+
+cd .config
+CWD=$(pwd)
+for f in * .*; do
+  [ "$f" = "." ] && continue
+  [ "$f" = ".." ] && continue
+  if [ -e $HOME/.config/$f ]; then
+    echo "Cannot deploy $f: $HOME/.config/$f already exists"
+  else
+    ln -s $CWD/$f $HOME/.config/$f
+  fi
+done
+cd ..
 
 echo "Deploying done"
