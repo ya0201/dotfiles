98432c9cc393b817ade15287c8cf2dccae4e2f9a
ya0201 <you@example.com>
2019-09-21 11:56:25 +0900

    Rename ubuntu setup script

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,5 +1,7 @@
 sudo apt install -y curl python3-pip zsh neovim peco
-sudo pip3 install neovim
+sudo pip3 install neovim xkeysnail
+
+which zsh >/dev/null 2>&1 && chsh -s $(which zsh)
 
 # install zplug
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
