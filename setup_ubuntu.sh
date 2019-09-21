9add7e97fdf34efa1e587aaa916ee5e0bbfc2dc5
ya0201 <you@example.com>
2019-09-21 15:50:50 +0900

    Writing japanese input setting script for ubuntu

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -7,3 +7,8 @@ which zsh >/dev/null 2>&1 && chsh -s $(which zsh)
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
 nvim -c PlugInstall -c qa
+
+# japanese input setting
+if which fcitx >/dev/null 2>&1; then
+  sudo apt install -y fcitx-mozc
+fi
