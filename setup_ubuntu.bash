c53822688aeb6f760b0c51407369c53a08245f90
ya0201 <you@example.com>
2020-09-12 21:33:54 +0900

    Add coc-settings.json to use mysnippet and fix setup script a little

--- setup_ubuntu.bash
+++ setup_ubuntu.bash
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 set -e
 
 [[ -z $XDG_CONFIG_HOME ]] && XDG_CONFIG_HOME="$HOME/.config"
@@ -5,6 +6,15 @@ set -e
 function check_is_installed() {
   which $@ >/dev/null 2>&1
 }
+
+
+# to install coc correctly
+# see: https://github.com/neoclide/coc.nvim/wiki/Install-coc.nvim
+## install node
+curl -sL install-node.now.sh/lts | sudo bash
+
+## install yarn with npm (required by neovim)
+sudo npm install -g yarn
 
 sudo apt install -y curl python3-pip zsh neovim peco tmux
 sudo pip3 install neovim xkeysnail
