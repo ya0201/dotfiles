dcc738fed192e15784634b6b0a356405a88e21f7
ya0201 <23146443+ya0201@users.noreply.github.com>
2025-06-09 16:08:08 +0900

    remove --break-system-packages because no such option

--- scripts/macos.zsh
+++ scripts/macos.zsh
@@ -31,7 +31,7 @@ echo "Deployment of dotfiles done."
 echo ''
 
 echo 'Installing neovim package for python3 and vim plugins...'
-pip3 install pynvim --user --break-system-packages
+pip3 install pynvim --user
 nvim -c PlugInstall -c qa
 echo 'Done.'
 echo ''
