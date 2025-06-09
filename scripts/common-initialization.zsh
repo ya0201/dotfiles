075b3936eeb294d3ded730e7ad9e49e02abf733f
ya0201 <23146443+ya0201@users.noreply.github.com>
2025-06-09 15:46:58 +0900

    Add homebrew binary to PATH in initialization

--- scripts/common-initialization.zsh
+++ scripts/common-initialization.zsh
@@ -9,3 +9,7 @@ export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-${HOME}/.config}
 mkdir -p $XDG_CONFIG_HOME
 export XDG_DATA_HOME=${XDG_DATA_HOME:-${HOME}/.local/share}
 mkdir -p $XDG_DATA_HOME
+
+# add homebrew binary to PATH temporarily
+## for apple sillicon macos
+export PATH="$PATH:/opt/homebrew/bin"
